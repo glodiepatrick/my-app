6de4f00e73f3cc5f2dab9ad69bc619805e649d56
Glody Patrick <elgloyo@gmail.com>
2022-12-08 02:19:47 +0200

    I added the word website after the namae of the company

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Visit Propeller
+          Visit Propeller Website
         </a>
       </header>
     </div>
